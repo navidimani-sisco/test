b36e8fcb4f409c15a3146d159156886326f8dfe7
navid imani <52500755+navidimani-sisco@users.noreply.github.com>
2021-02-28 23:23:29 +0330

    Add files via upload

---
+++ includes/functions.php
@@ -0,0 +1,26 @@
+<?php
+include 'DB.php';
+
+if( ! function_exists('alertTemplate') ){
+	function alertTemplate( $text , $type = 'error' ){
+		switch( $type ){
+			case 'success': break;
+			case 'warning': break;
+			case 'error':	$type = 'danger';
+		}
+		$alert = "
+				<article class = 'alert alert-{$type} alert-dismissible fade show' role='alert'>
+					{$text}
+					<button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+				</article>";
+		return $alert;	
+	}
+}
+
+
+if( ! function_exists('redirect') ){
+	function redirect( $address ){
+		header("Location: {$address}"); // دستور به مرورگر براي ريدایرکت به آدرس
+		exit();
+	}
+}
---
+++ includes/settings.php
@@ -0,0 +1,19 @@
+<?php
+if( ! defined('DBHOST') )
+	define( 'DBHOST', 'localhost' );
+if( ! defined('DBUSER') )
+	define( 'DBUSER', 'root' );
+if( ! defined('DBPASS') )
+	define( 'DBPASS', '' );
+if( ! defined('DBNAME') )
+	define( 'DBNAME', 'mobtani' );
+	
+if( ! defined('CHARSET') )
+	define( 'CHARSET', 'utf8mb4' );
+
+$dbHost =	DBHOST;
+$dbUser =	DBUSER;
+$dbPass =	DBPASS;
+$dbname =	DBNAME;
+
+$charset =	CHARSET;
